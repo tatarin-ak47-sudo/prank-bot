592b227f34c30f5377fd23b8c94a8e3835989139
tatarin-ak47-sudo <tatarin-ak47@mail.ru>
2026-02-17 19:44:34 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -199,7 +199,8 @@ def health():
     return jsonify({"status": "alive"})
 
 def run_web_server():
-    app.run(host='0.0.0.0', port=5000, debug=False, use_reloader=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False, use_reloader=False)
 
 # ============================================
 # ============ БОТ ===========================
